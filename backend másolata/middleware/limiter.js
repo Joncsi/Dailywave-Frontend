9fd3575e698f674991fc2a10815da06dd1e425ca
Joncs <scilling.janos.attila.1046@dszcbaross.edu.hu>
2025-01-29 08:35:44 +0100

    only frontend

--- backend másolata/middleware/limiter.js
+++
@@ -1,8 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-    windowMs: 1000 * 60 * 15,
-    max: 1000
-});
-
-module.exports = limiter;
--- backend másolata/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { config } = require('./config/dotenvConfig');
-
-app.listen(config.PORT, config.HOSTNAME, () => {
-    console.log(`IP: http://${config.HOSTNAME}:${config.PORT}`);
-});
